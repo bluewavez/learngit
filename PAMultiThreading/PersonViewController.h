ee302890c38d6a113a1536a0275be1924e5b55f3
朱永杰 <zhuyongjie@zhuyongjiedeMacBook.local>
2019-04-20 14:30:37 +0800

    add file

---
+++ PAMultiThreading/PersonViewController.h
@@ -0,0 +1,17 @@
+//
+//  PersonViewController.h
+//  PAMultiThreading
+//
+//  Created by 朱永杰 on 2019/4/20.
+//  Copyright © 2019年 individual. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PersonViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PAMultiThreading/PersonViewController.m
@@ -0,0 +1,32 @@
+//
+//  PersonViewController.m
+//  PAMultiThreading
+//
+//  Created by 朱永杰 on 2019/4/20.
+//  Copyright © 2019年 individual. All rights reserved.
+//
+
+#import "PersonViewController.h"
+
+@interface PersonViewController ()
+
+@end
+
+@implementation PersonViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
